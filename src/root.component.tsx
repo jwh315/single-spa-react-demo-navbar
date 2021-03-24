8c8850a2f3472a351f642a6c56588297885b9452
Jacob Holmes <jacob.holmes@coxautoinc.com>
2021-03-24 09:39:38 -0700

    initial commit

---
+++ src/root.component.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { Link, navigate } from "@reach/router";
+// @ts-ignore
+import { IUser, IRootProps } from "@cd/models";
+
+export const links = [
+  {
+    name: "Home",
+    href: "/dashboard",
+  },
+  {
+    name: "Test",
+    href: "/test",
+  },
+];
+
+const Root = (props: IRootProps) => {
+  const [user, setUser] = useState(props.user);
+
+  props.subscribe("USER_DATA_EVENT", (msg, data) => {
+    setUser(data);
+  });
+
+  const tabs = links.map((link) => (
+    <Link key={link.href} className="p-6" to={link.href}>
+      {link.name}
+    </Link>
+  ));
+
+  const navigation = Object.keys(user).length ? (
+    <div className="h-16 flex items-center justify-between px-6 bg-primary text-white">
+      <div className="flex items-center justify-between">{tabs}</div>
+      <div className="flex items-center justify-between">
+        <span>Welcome, {user.fullName}</span>
+        <span className="px-2">|</span>
+        <Link key="/logout" to="/logout">
+          Logout
+        </Link>
+      </div>
+    </div>
+  ) : null;
+
+  return navigation;
+};
+
+export default React.memo(Root);
